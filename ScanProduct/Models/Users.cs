f214e7687f707f61cc9b514f4f363d2076944c93
lama <tunglamvo1509@gmail.com>
2024-03-22 10:03:56 +0700

    push

---
+++ ScanProduct/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanProduct.Models
+{
+    public  class Users
+    {
+        public string UserdId { get; set; }
+        public string Name { get; set; }
+        public int Point { get; set; }
+        
+    }
+}
